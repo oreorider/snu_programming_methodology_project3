29b0567febe4fcede44c763c12f177c550ab3526
oreorider <yjkim308@gmail.com>
2021-12-01 00:00:04 +0900

    normalcontroller work

--- NormalController.cpp
+++ NormalController.cpp
@@ -17,11 +17,33 @@
  */
 bool NormalController::stackFood(const string name, intPair foodSize, int exp)
 {
-    /**
-     * ===============================================
-     * ======== TODO: Implement this function ========
-     * ===============================================
-     */
+    Food food_to_insert(name, foodSize, exp);
+    //check if space avail on shelves
+    int avail_x, avail_y;
+    int avail_space_start_x, avail_space_end_x;
+    int shelf_height=0;
+    for(auto &each_shelf : shelves){//for each shelf
+        for(auto &each_foodptr : each_shelf.vec){//for each foodptr/foodinfridge
+            avail_space_start_x=each_foodptr->getPos().first+(each_foodptr->getSize().first);
+            avail_space_end_x;
+
+            auto nextElement=*(&each_foodptr+1);
+            if(&nextElement==&each_shelf.vec.back()){//if last element of vector
+                avail_space_end_x=size.first;
+            }
+            else{
+                avail_space_end_x=nextElement->getPos().first;
+            }
+            avail_x=avail_space_start_x-avail_space_end_x;
+            if(avail_x>foodSize.first){//enough space in x, assume height of shelf>height of food always
+                //insert food into shelf @ avail_space_start_x
+                
+                FoodPtr new_food=new FoodInFridge(food_to_insert, avail_space_end_x, shelf_height);
+                each_shelf.vec.push_back(new_food);
+            }
+        }
+        shelf_height+=each_shelf.height;
+    }
 
     return false;
 }
