999da20ed36be0fb2b33601bb26535bcf4419c15
oreorider <yjkim308@gmail.com>
2021-12-07 21:30:00 +0900

    normalcontroller updated

--- NormalController.cpp
+++ NormalController.cpp
@@ -18,18 +18,19 @@
 bool NormalController::stackFood(const string name, intPair foodSize, int exp)
 {
     Food food_to_insert(name, foodSize, exp);
+    bool inserted=false;
     //check if space avail on shelves
     int avail_x, avail_y;
     int avail_space_start_x, avail_space_end_x;
     int shelf_height=0;
     for(auto &each_shelf : shelves){//for each shelf
-        for(auto &each_foodptr : each_shelf.vec){//for each foodptr/foodinfridge
+        for(auto &each_foodptr : each_shelf.vec){//for each foodptr/foodinfridge that is in the shelf
             avail_space_start_x=each_foodptr->getPos().first+(each_foodptr->getSize().first);
             avail_space_end_x;
 
             auto nextElement=*(&each_foodptr+1);
             if(&nextElement==&each_shelf.vec.back()){//if last element of vector
-                avail_space_end_x=size.first;
+                avail_space_end_x=size.first;//set end_x the 'wall' of fridge
             }
             else{
                 avail_space_end_x=nextElement->getPos().first;
@@ -37,15 +38,26 @@ bool NormalController::stackFood(const string name, intPair foodSize, int exp)
             avail_x=avail_space_start_x-avail_space_end_x;
             if(avail_x>foodSize.first){//enough space in x, assume height of shelf>height of food always
                 //insert food into shelf @ avail_space_start_x
-                
                 FoodPtr new_food=new FoodInFridge(food_to_insert, avail_space_end_x, shelf_height);
-                each_shelf.vec.push_back(new_food);
+                each_shelf.vec.push_back(new_food);//food added to shelf
+                if(foodList.find(name)==foodList.end()){//food does not exist in foodlist
+                    vector<FoodPtr> v{new_food};
+                    foodList.insert(make_pair(name, v));//add the new food to foodlist
+                }
+                else{//food already exsists in foodList
+                    foodList[name].push_back(new_food);//append foodptr to foodlist
+                }
+                inserted=true;
             }
         }
         shelf_height+=each_shelf.height;
     }
-
-    return false;
+    if(inserted){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 
@@ -59,6 +71,11 @@ bool NormalController::stackFood(const string name, intPair foodSize, int exp)
  */
 bool NormalController::popFood(const string food_name)
 {
+    if(foodList.find(food_name)==foodList.end()){//food not in foodList.
+        return false;
+    }
+    //assuming now the food is inside the foodList
+    auto v = foodList[food_name];
     /**
      * ===============================================
      * ======== TODO: Implement this function ========
